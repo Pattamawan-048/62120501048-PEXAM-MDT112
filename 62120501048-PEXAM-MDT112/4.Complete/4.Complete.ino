11bd78ab0b97c39444260c74031b30c0c3455299
Pattamawan62120501048 <pattamawan.p@mail.kmutt.ac.th>
2020-07-04 14:46:15 +0700

    4.complete

---
+++ 62120501048-PEXAM-MDT112/4.Complete/4.Complete.ino
@@ -0,0 +1,17 @@
+#include <Stepper.h>
+
+#define STEPS_PER_360_DEGREE 2038
+
+Stepper stepper(STEPS_PER_360_DEGREE, 8, 10, 9, 11); // IN1 IN2 IN3 IN4
+
+void setup() {
+  Serial.begin(9600);
+
+  stepper.setSpeed(16); // Unit as RPM (revolutions per minute) MIN 0-16 RPM
+  Serial.println("Step CCW 180  Degrees");
+  stepper.step(STEPS_PER_360_DEGREE/2);
+  delay(600);
+}
+void loop() {
+
+}
