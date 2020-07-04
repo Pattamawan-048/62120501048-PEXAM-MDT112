ae724313676429893475d4e53edb9bd17f9189f0
Pattamawan62120501048 <pattamawan.p@mail.kmutt.ac.th>
2020-07-04 15:20:10 +0700

    1.completeedit

--- 62120501048-PEXAM-MDT112/1.Complete/1.Complete.ino
+++ 62120501048-PEXAM-MDT112/1.Complete/1.Complete.ino
@@ -3,9 +3,7 @@ int buzzer = 8;
 void setup() {
      pinMode(buzzer,OUTPUT);
      tone(buzzer, 900,600); 
-     delay(100); 
      tone(buzzer,900,600); 
-     delay(100); 
      }
    void loop() {
    
