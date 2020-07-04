7116d2cd661289e13198396a06323f1e14011f20
Pattamawan62120501048 <pattamawan.p@mail.kmutt.ac.th>
2020-07-04 15:20:31 +0700

    6.completeedit

--- 62120501048-PEXAM-MDT112/6.Complete/6.Complete.ino
+++ 62120501048-PEXAM-MDT112/6.Complete/6.Complete.ino
@@ -4,9 +4,7 @@ void setup() {
      pinMode(buzzer,OUTPUT);
       delay(400);
      tone(buzzer, 600,400); 
-    delay(100); 
      tone(buzzer,600,400); 
-    delay(100); 
      }
    void loop() {
 }
